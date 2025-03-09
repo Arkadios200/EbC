e3e1324aad0adab874e4b3a675e370233517f173
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-03-09 08:02:48 -0500

    Create EbC-24-03ab.swift

---
+++ 2024/EbC-24-03ab.swift
@@ -0,0 +1,39 @@
+extension Collection {
+  subscript(safe index: Index) -> Element? {
+    return indices.contains(index) ? self[index] : nil
+  }
+}
+
+var grid = [[Int]]()
+while let line = readLine() {
+  grid.append(line.map { $0 == "#" ? 1 : 0 } )
+}
+
+var newGrid = grid
+var n = 1
+outer: while true {
+  for i in grid.indices {
+    for j in grid[i].indices {
+      let temp: [Int] = [
+        grid[i][j],
+        grid[i][safe: j+1] ?? 0,
+        grid[safe: i+1]?[j] ?? 0,
+        grid[i][safe: j-1] ?? 0,
+        grid[safe: i-1]?[j] ?? 0
+      ]
+      if temp.allSatisfy( { $0 == n } ) {
+        newGrid[i][j] += 1
+      }
+    }
+  }
+
+  if grid == newGrid {
+    break outer
+  } else {
+    n += 1
+    grid = newGrid
+  }
+}
+
+var total = grid.joined().reduce(0, +)
+print(total)
