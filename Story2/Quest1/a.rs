12f5f9e98fa0ea27cd132b48409a42b842b10a7f
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-08-31 16:17:54 -0400

    Update a.rs

--- Story2/Quest1/a.rs
+++ Story2/Quest1/a.rs
@@ -4,53 +4,82 @@ use std::collections::HashSet;
 fn main() {
   let input = fs::read_to_string("input.txt").unwrap();
 
-  let (nails, coins) = process(&input);
-  let width = nails.iter().map(|&p| p.x).max().unwrap();
-  let height = nails.iter().map(|&p| p.y).max().unwrap();
+  let (board, coins) = process(&input);
 
-  let mut total = 0;
-  for (x, coin) in (1..=9).map(|x| (x - 1) * 2).zip(coins) {
-    let mut p = Point { x, y: 0 };
-    let mut c = coin.chars().cycle();
-
-    while p.y <= height {
-      if nails.contains(&p) {
-        let d = c.next().unwrap();
-        p.x += match p.x {
-          0 => 1,
-          _ if p.x == width => -1,
-          _ => if d == 'R' { 1 } else { -1 },
-        }
-      }
-
-      p.y += 1;
-    }
-
-    let score = (p.x / 2 + 1) * 2 - (x / 2 + 1);
-    total += if score > 0 { score } else { 0 };
-  }
+  let total: i32 = (1i32..).zip(coins).map(|(n, coin)| 2 * coin.simulate(&board, n) - n).filter(|&score| score > 0).sum();
 
   println!("{total}");
 }
 
-fn process(input: &str) -> (HashSet<Point>, Vec<String>) {
+fn process(input: &str) -> (Board, Vec<Coin>) {
   let mut blocks = input.split("\n\n");
 
-  let grid = blocks.next().unwrap();
   let mut nails: HashSet<Point> = HashSet::new();
-  for (i, line) in grid.lines().enumerate() {
+  for (i, line) in blocks.next().unwrap().lines().enumerate() {
     for (j, c) in line.chars().enumerate() {
-      if c == '*' { nails.insert(Point { x: j as i32, y: i as i32 }); }
+      if c == '*' {
+        nails.insert(Point { x: j as i32, y: i as i32 });
+      }
     }
   }
 
-  let coins: Vec<String> = blocks.next().unwrap().lines().map(|s| String::from(s)).collect();
+  let coins: Vec<Coin> = blocks.next().unwrap().lines().map(|line| Coin { dirs: line.chars().map(|c| match c { 'L' => -1, 'R' => 1, _ => panic!(), }).collect() }).collect();
 
-  (nails, coins)
+  (Board { nails }, coins)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
   x: i32,
   y: i32,
+}
+
+#[derive(Clone)]
+struct Board {
+  nails: HashSet<Point>,
+}
+
+impl Board {
+  fn width(&self) -> i32 {
+    self.nails.iter().map(|&p| p.x).max().unwrap()
+  }
+
+  fn height(&self) -> i32 {
+    self.nails.iter().map(|&p| p.y).max().unwrap()
+  }
+
+  fn slot(x: i32) -> i32 {
+    x / 2 + 1
+  }
+
+  fn x(slot: i32) -> i32 {
+    (slot - 1) * 2
+  }
+}
+
+#[derive(Clone)]
+struct Coin {
+  dirs: Vec<i32>
+}
+
+impl Coin {
+  fn simulate(&self, board: &Board, slot: i32) -> i32 {
+    let mut pos = Point { x: Board::x(slot), y: 0 };
+    let mut dirs = self.dirs.iter().cycle();
+
+    while pos.y <= board.height() {
+      if board.nails.contains(&pos) {
+        let d = *dirs.next().unwrap();
+        pos.x += match pos.x {
+          0 => 1,
+          _ if pos.x == board.width() => -1,
+          _ => d,
+        }
+      }
+
+      pos.y += 1;
+    }
+
+    Board::slot(pos.x)
+  }
 }
