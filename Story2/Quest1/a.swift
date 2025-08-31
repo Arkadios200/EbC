5481ae348f30af810cffd20d91b3c1a1fe91e906
Arkadios <58800860+Arkadios200@users.noreply.github.com>
2025-08-31 10:21:12 -0400

    Create a.swift

---
+++ Story2/Quest1/a.swift
@@ -0,0 +1,87 @@
+struct Point: Equatable, Hashable {
+  var x: Int
+  var y: Int
+
+  init(x: Int, y: Int) {
+    self.x = x
+    self.y = y
+  }
+}
+
+struct Board {
+  let nails: Set<Point>
+  let width: Int
+  let height: Int
+
+  static let slot: (Int) -> Int = { $0 / 2 + 1 }
+  static let x: (Int) -> Int = { ($0 - 1) * 2 }
+
+  init(_ nails: Set<Point>) {
+    self.nails = nails
+    self.width = nails.map { $0.x }.max()!
+    self.height = nails.map { $0.y }.max()!
+  }
+}
+
+struct Coin {
+  let dirs: [Character]
+
+  init(dirs: [Character]) {
+    self.dirs = dirs
+  }
+
+  func simulate(_ board: Board, slot: Int) -> Int {
+    var pos = Point(x: Board.x(slot), y: 0)
+
+    var i = 0
+    while pos.y <= board.height {
+      if board.nails.contains(pos) {
+        pos.x += {
+          switch pos.x {
+            case 0: return 1
+            case board.width: return -1
+            default: 
+              return dirs[i] == "R" ? 1 : -1
+          }
+        }()
+        i = (i + 1) % dirs.count
+      }
+      pos.y += 1
+    }
+
+    return Board.slot(pos.x)
+  }
+}
+
+func getInput() -> (Board, [Coin]) {
+  var grid: [[Character]] = []
+  while let line = readLine(), !line.isEmpty {
+    grid.append(Array(line))
+  }
+
+  var board: Set<Point> = []
+  for (i, line) in grid.enumerated() {
+    for (j, c) in line.enumerated() {
+      if c == "*" { board.insert(Point(x: j, y: i)) }
+    }
+  }
+
+  var coins: [Coin] = []
+  while let line = readLine() {
+    coins.append(Coin(dirs: Array(line)))
+  }
+
+  return (Board(board), coins)
+}
+
+let (board, coins) = getInput()
+
+var total = 0
+for (n, coin) in zip(1..., coins) {
+  let initial = n
+  let final = coin.simulate(board, slot: n)
+  let score = (final * 2) - initial
+
+  if score > 0 { total += score }
+}
+print(total)
